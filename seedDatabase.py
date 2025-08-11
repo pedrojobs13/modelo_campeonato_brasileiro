0f3eaf0a46295b7de33fce698c4404311ea9b123
Pedro Lucas Gomes <pedrolucasgomesdeoliveira13@gmail.com>
2025-08-11 16:04:24 -0300

    feat: adicionando docker ao projeto

--- seedDatabase.py
+++ seedDatabase.py
@@ -10,22 +10,21 @@ def connectDatabase():
                         user=os.getenv("USER"),
                         password=os.getenv("PASSWORD"),
                         port="5432")
-        return conn.cursor()
+        return conn
     except postgres.Error as e:
         print(f"Error connecting to PostgreSQL: {e}")
     
 def createCartaoDatabaseIfNotExist(cursor):
-        
     pathCartoes = 'campeonato-brasileiro-cartoes.csv'
-    colNames = ["partida_id", "rodata", "clube", "cartao", "atleta", "num_camisa", "posicao", "minuto"]
-    cartoesCsv= pd.read_csv(pathCartoes, names=colNames)
+    colNames = ("partida_id", "rodata", "clube", "cartao", "atleta", "num_camisa", "posicao", "minuto")
+    table_name = "cartoes"
+    cartoesCsv= pd.read_csv(pathCartoes, names=colNames,header=None, skiprows=1)
     
-    nameColumnsDatabase = cartoesCsv.head()
     create_table_sql = f"""
-        CREATE TABLE IF NOT EXISTS users (
+        CREATE TABLE IF NOT EXISTS {table_name} (
             id SERIAL PRIMARY KEY,
-            {colNames[0]} BIGINT NOT NULL,
-            {colNames[1]} BIGINT NOT NULL,
+            {colNames[0]} VARCHAR(100) NOT NULL,
+            {colNames[1]} VARCHAR(100) NOT NULL,
             {colNames[2]} VARCHAR(100),
             {colNames[3]} VARCHAR(100),
             {colNames[4]} VARCHAR(100),
@@ -34,14 +33,22 @@ def createCartaoDatabaseIfNotExist(cursor):
             {colNames[7]} VARCHAR(100) 
         )    
     """
-    print(create_table_sql)
     
-    cursor.execute(create_table_sql)
+    columns = [col.replace(" ", "_") for col in cartoesCsv.columns]
+    placeholders = ", ".join(["%s"] * len(columns))
+    insert_sql = f"INSERT INTO {table_name} ({', '.join(columns)}) VALUES ({placeholders})"
+    
+    for _, row in cartoesCsv.iterrows():
+        cursor.execute(insert_sql, tuple(row))
+    
 
 def main():
     load_dotenv()
-    cursor = connectDatabase()
-    createCartaoDatabaseIfNotExist(cursor)
-
-    cursor.close()
+    conn = connectDatabase()
+    
+    if conn:
+        createCartaoDatabaseIfNotExist(conn.cursor())
+        conn.commit()
+        conn.close()
+        
 main()
